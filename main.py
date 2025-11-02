cd583aeb514c65b3f78dcc663ee674d03530ccc1
seven7een <0x86239@protonmail.com>
2025-11-02 20:56:06 +0000

    Add player and player movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
 
@@ -9,7 +10,22 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+
+    # Setting up the screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Timing vars
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Setting up groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    # Player vars
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
@@ -17,7 +33,15 @@ def main():
                 return
 
         screen.fill((0,0,0))
+
+        updatable.update(dt)
+
+        for d in drawable:
+            d.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
